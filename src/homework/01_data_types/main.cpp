0bda6e2df7ca6c5dee59661c2a66ba23c285d1bf
quintanillaje <jessica.quintanilla2@g.austincc.edu>
2022-09-10 19:38:29 +0000

    completed homework 1

--- src/homework/01_data_types/main.cpp
+++ src/homework/01_data_types/main.cpp
@@ -7,9 +7,18 @@
 /*
 Call multiply_numbers with 10 and 10 parameter values and display function result
 */
+#include <iostream>
+using std::cin;
+#include "data_types.h"
+
 int main()
 {
-	
-
+	int num;
+	cin>> num;
+	int result = multiply_numbers(num);
+	std::cout<< result<< std::endl;
+	int num1 = 4;	
+	result = multiply_numbers(num1);
+	std::cout<<result<<std::endl;
 	return 0;
 }
--- test/homework/01_data_types/01_data_types_tests.cpp
+++ test/homework/01_data_types/01_data_types_tests.cpp
@@ -1,7 +1,8 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "data_types.h"
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
 }
 
@@ -9,5 +10,19 @@ TEST_CASE("Verify sum_numbers function")
 {
 	REQUIRE(add_numbers(5, 5) == 10);
 	REQUIRE(add_numbers(10, 10) == 20);
+}
+
+
+#include "data_types.cpp"
+
+TEST_CASE("Multiply Numbers", "verification") 
+{
+	REQUIRE(true == true);
+}
+
+TEST_CASE("verify the product of multiply_numbers function")
+{
+	REQUIRE(multiply_numbers(10) == 50);
+	REQUIRE(multiply_numbers(2) == 10);
 }
 
